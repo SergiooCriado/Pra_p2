c8b21939db319a381de85b363e9cdb15aa746bd4
SergiooCriado <scrirei@upv.edu.es>
2025-10-21 19:36:53 +0200

    Update DyV.h

--- DyV.h
+++ DyV.h
@@ -1,80 +1,57 @@
-#ifndef DYV_H
-#define DYV_H
-
 #include <iostream>
-#include <algorithm> 
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-template<typename T>
-void intercambiar(T v[], int i, int j) {
-    T aux = v[i];
-    v[i] = v[j];
-    v[j] = aux;
+template<typename TipoDato>
+int BuscarElemento(TipoDato valorBuscado, const vector<TipoDato>& arreglo, int inicio, int final){
+	if(inicio > final){
+		return -1;
+	}
+	int puntoMedio = (inicio + final) / 2;
+	if(arreglo[puntoMedio] == valorBuscado){
+		return puntoMedio;
+	}else if(arreglo[puntoMedio] > valorBuscado){
+		return BuscarElemento(valorBuscado, arreglo, inicio, puntoMedio-1);
+	}else{
+		return BuscarElemento(valorBuscado, arreglo, puntoMedio+1, final);
+	}
 }
 
-template<typename T>
-int Partition(T V[], int ini, int fin) {
-    T x = V[fin];
-    int i = ini;
-    int j;
-
-    for(j = ini; j < fin; j++) {
-        if(V[j] <= x) {
-            intercambiar(V, i, j);
-            i = i + 1;
-        }
-    }
-    
-    intercambiar(V, i, fin);
-    return i;
+template<typename TipoDato>
+int BuscarElemento_DEC(TipoDato valorBuscado, const vector<TipoDato>& arreglo, int inicio, int final){
+        if(inicio > final){
+                return -1;
+        }
+        int puntoMedio = (inicio + final) / 2;
+        if(arreglo[puntoMedio] == valorBuscado){
+                return puntoMedio;
+        }else if(arreglo[puntoMedio] < valorBuscado){
+                return BuscarElemento_DEC(valorBuscado, arreglo, inicio, puntoMedio-1);
+        }else{
+                return BuscarElemento_DEC(valorBuscado, arreglo, puntoMedio+1, final);
+        }
 }
 
-template<typename T>
-void QuickSort(T V[], int ini, int fin) {
-    if (ini < fin) {
-        int pivot = Partition(V, ini, fin);
-        QuickSort(V, ini, pivot - 1);
-        QuickSort(V, pivot + 1, fin);
-    }
+template <typename TipoDato>
+int Delimitar(vector <TipoDato>& lista, int inferior, int superior){
+	TipoDato elementoPivote = lista[superior];
+	int indiceMenor = inferior;
+	for(int indiceActual=inferior; indiceActual < superior; indiceActual++){
+		if(lista[indiceActual] <= elementoPivote){
+			swap(lista[indiceMenor], lista[indiceActual]);
+			indiceMenor++;
+		}
+	}	
+	swap(lista[indiceMenor], lista[superior]);
+	return indiceMenor;
 }
 
-template<typename T>
-int BusquedaBinaria(T& x, T v[], int ini, int fin){
-    if(ini > fin){
-        return -1;
-    }
-
-    int medio = ini + (fin - ini) / 2;
-
-    if(v[medio] == x){
-        return medio;
-    }
-    else if(v[medio] > x){
-        return BusquedaBinaria(x, v, ini, medio - 1);
-    }
-    else{
-        return BusquedaBinaria(x, v, medio + 1, fin);
-    }
-} 
-
-template<typename T>
-int BusquedaBinariaInv(T& x, T v[], int ini, int fin){
-    if(ini > fin){
-        return -1;
-    }
-
-    int medio = ini + (fin - ini) / 2;
-
-    if(v[medio] == x){
-        return medio;
-    }
-    else if(v[medio] < x){ 
-        return BusquedaBinariaInv(x, v, ini, medio - 1); 
-    }
-    else{ 
-        return BusquedaBinariaInv(x, v, medio + 1, fin); 
-    }
+template<typename TipoDato>
+void OrdenarRapido(vector <TipoDato>& lista, int inferior, int superior){
+        if(inferior < superior){
+                int puntoDivision = Delimitar(lista, inferior ,superior);
+                OrdenarRapido(lista, inferior, puntoDivision - 1);
+                OrdenarRapido(lista, puntoDivision + 1, superior);
+        }
 }
-
-#endif
